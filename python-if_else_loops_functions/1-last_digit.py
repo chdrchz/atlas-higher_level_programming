71fba53f1b3faf85883c4becfee3abb9a3c7e3d5
chdrchz <saavannaa.rose@gmail.com>
2024-01-02 11:44:30 -0800

    more ifs!!!!!!!!!

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+
 import random
 number = random.randint(-10000, 10000)
-new_num = abs(number) % 10
+
+if number < 0:
+    new_num = -(abs(number) % 10)
+else:
+    new_num = abs(number) % 10
+
 output = f"Last digit of {number} is {new_num}"
 
 if new_num > 0:
