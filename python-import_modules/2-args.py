21042ca2f76c32410074d80cf1f05013a9c0c085
chdrchz <saavannaa.rose@gmail.com>
2024-01-03 12:18:11 -0800

    adding elif

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     if argc == 0:
         print("0 arguments.")
-    if argc == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
         print("{} arguments".format(argc))
