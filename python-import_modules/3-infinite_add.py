b29dc9f3dea8f8dc6aefadbace926aa931caa634
chdrchz <saavannaa.rose@gmail.com>
2024-01-03 12:43:43 -0800

    test

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-from sys import argv
-
 if __name__ == "__main__":
+
+    import sys
+
+    sums = 0
     sums = sum(int(arg) for arg in argv[1:] if arg.isdigit())
     print(sums)
