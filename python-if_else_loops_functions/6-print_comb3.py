c113705ba44d267aaf1731b2fff39878728b9584
chdrchz <saavannaa.rose@gmail.com>
2024-01-03 08:56:54 -0800

    fixing checks

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i:02d}, {j:02d}")
+        if i * 10 + j < 89:
+            print("{:02d}".format(i * 10 + j), end=", ")
+        else:
+            print("{:02d}".format(i * 10 + j))
