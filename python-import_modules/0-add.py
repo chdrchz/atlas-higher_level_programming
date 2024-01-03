067ed5514a88c7f734f14473f55feb1771da2623
chdrchz <saavannaa.rose@gmail.com>
2024-01-03 10:27:27 -0800

    reverting to original with proper indentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,5 +5,6 @@ from add_0 import add
 a = 1
 b = 2
 
-if __name__ == "__main__"
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
