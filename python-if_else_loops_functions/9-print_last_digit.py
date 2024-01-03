c4a11afed0a0f0a9f8194d5a759b8caedd84983f
chdrchz <saavannaa.rose@gmail.com>
2024-01-03 09:11:17 -0800

    I cant see the checks so I am guessing here

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_num = number % 10
+    if number < 0:
+        last_num = -(abs(number) % 10)
+    else:
+        last_num = abs(number) % 10
     return last_num
