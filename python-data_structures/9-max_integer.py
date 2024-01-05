0011429a59a81397b15c17d1940e046c0d31f0e2
chdrchz <saavannaa.rose@gmail.com>
2024-01-05 13:50:49 -0800

    added loop

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,4 +3,11 @@ def max_integer(my_list=[]):
 
     if not my_list:
         return None
-    max_number = sorted(my_list, reverse=True)[0]
+
+    max_num = my_list[0]
+
+    for number in my_list:
+        if number > max_num:
+            max_num = number
+
+    return max_num
