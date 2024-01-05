adc40428cef57c92986035507dba702e9d276d7f
chdrchz <saavannaa.rose@gmail.com>
2024-01-05 08:54:25 -0800

    changing small things, removing loop

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/python3t 
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0 or idx >= length:
+    if idx < 0 or idx >= len(my_list)
         return None
-    if idx == i:
-        return my_list[i]
+    return my_list[idx]
