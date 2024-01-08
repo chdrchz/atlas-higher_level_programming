0c99e8539ef0b18695b52bc3afb3d57a5d2df8a6
chdrchz <saavannaa.rose@gmail.com>
2024-01-08 14:37:01 -0800

    fixing len(matrix) in loop

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -6,8 +6,8 @@ def square_matrix_simple(matrix=[]):
 
     matrix_sq = [[0] * len(matrix[0]) for _ in range(len(matrix))]
 
-    for i in range(matrix):
-        for j in range(matrix[0]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
             matrix[i][j] = matrix[i][j] ** 2
 
     return matrix_sq
