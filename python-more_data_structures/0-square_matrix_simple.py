bda5c881acc2c6a7f9df82d8716584d41a1ed461
chdrchz <saavannaa.rose@gmail.com>
2024-01-08 14:49:53 -0800

    testing logic change - apending to each row and column on iteration

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
-    if not matrix:
-        return[]
-
-    matrix_sq = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            matrix[i][j] = matrix[i][j] ** 2
-
+    matrix_sq = []
+    for row in matrix:
+        row_sq = []
+        for num in row:
+            row_sq.append(num ** 2)
+        matrix_sq.append(row_sq)
     return matrix_sq
