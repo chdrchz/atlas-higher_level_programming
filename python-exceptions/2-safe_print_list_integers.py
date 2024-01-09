8fd99c7abed9402cbab7f7d1653f722ddddf06f4
chdrchz <saavannaa.rose@gmail.com>
2024-01-09 12:37:04 -0800

    making changes to except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for item in my_list[:x]:
+        for item in range(x):
             value = my_list[item]
             print("{:d}".format(value), end="")
             if isinstance(value, int):
                 count += 1
-    except IndexError:
+    except (ValueError, IndexError):
         pass
     print()
     return count
