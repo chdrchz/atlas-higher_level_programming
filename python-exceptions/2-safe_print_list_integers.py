d9299b4c6987e7affdb36739696d8a6817846f81
chdrchz <saavannaa.rose@gmail.com>
2024-01-09 12:49:01 -0800

    changed logic around. 2 try and except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,13 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for item in range(x):
-            value = my_list[item]
-            print("{:d}".format(value), end="")
-            if isinstance(value, int):
+            try:
+                value = my_list[item]
+                print("{:d}".format(int(value)), end="")
                 count += 1
-    except (ValueError, IndexError):
+            except (ValueError, TypeError):
+                pass
+    except IndexError:
         pass
     print()
     return count
