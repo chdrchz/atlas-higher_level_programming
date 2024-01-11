7ae6568d169575f4fa9488b31481efd96f1f1c24
chdrchz <saavannaa.rose@gmail.com>
2024-01-11 17:49:38 +0000

    test

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,3 @@ class Square:
         def area(self):
             """Calculates area of a square"""
             return(self.__size**2)
-
-        @property
-        def size(self):
-            return(self.__size)
