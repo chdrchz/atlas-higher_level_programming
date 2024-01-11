3b0f89d74b97e5bad16dd59db4828ab0c20cecfa
chdrchz <saavannaa.rose@gmail.com>
2024-01-11 18:18:02 +0000

    fixing dmall errors

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class named square"""
+"""Write a class Square that defines a square:"""
 
 
 class Square:
-    """This is a class that calculates the area of a square"""
+    """This class defines a square"""
     def __init__(self, size=0):
+        """This initializes a simple square"""
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("size must not be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return (self.__size**2)
+        """area of the square"""
+        return(self.__size**2)
 
     @property
     def size(self):
-        return (self.__size)
+        return(self.__size)
 
     @size.setter
     def size(self, value):
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
