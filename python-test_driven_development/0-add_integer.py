c75d67042cd0b1580b0933bf43f4fb9d727e7b96
chdrchz <saavannaa.rose@gmail.com>
2024-01-14 18:12:33 +0000

    changing if statements to only check against a and b by themselves

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,8 +3,8 @@
 
 def add_integer(a, b=98):
     """This function adds two integers and returns the result"""
-    if not isinstance(a, int) or not isinstance(b, float):
+    if not isinstance(a, int):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or not isinstance(a, float):
+    if not isinstance(b, int):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
