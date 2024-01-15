d5745f8b234f6219f3c74384a722c470d6bfd588
chdrchz <saavannaa.rose@gmail.com>
2024-01-15 20:28:43 +0000

    adding function that prints a name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This file defines a function that prints a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function prints a name"""
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        TypeError(
+                "first_name must be a string or last_name must be a string"
+        )
+    print(f"My name is {first_name} {last_name}")
