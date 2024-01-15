c2972d91c8e9521c497b45596932750a3814f8fb
chdrchz <saavannaa.rose@gmail.com>
2024-01-15 20:04:14 +0000

    fixing line character count

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,7 +9,9 @@ def matrix_divided(matrix, div):
             isinstance(element, (int, float)) for element in row
         ) for row in matrix
     ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
