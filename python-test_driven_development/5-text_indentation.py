83b1d310a6cf37f79693fed35d6c4293b05b0731
chdrchz <saavannaa.rose@gmail.com>
2024-01-15 21:25:49 +0000

    function that prints after exncountering special characters

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This file defines a function that prints after characters"""
+
+
+def text_indentation(text):
+    """This function prints after special characters"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    lines = text.split()
+    for line in lines:
+        print(line.strip())
