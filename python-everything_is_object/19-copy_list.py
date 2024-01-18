92fcede0681c7c47fddd07a48e02b5e357839dd8
chdrchz <saavannaa.rose@gmail.com>
2024-01-18 16:23:17 +0000

    removing documentation and shebang? weird

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""This module defines a function that returns a copy of a list"""
-
-
 def copy_list(a_list):
-    """This function returns a copy of a list"""
     b_list = a_list
     return b_list
