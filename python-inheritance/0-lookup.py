bb74a20b45cf74d14ae07d8f9b7e6da68ee3f500
chdrchz <saavannaa.rose@gmail.com>
2024-01-19 02:56:51 +0000

    adding a function that returns a list

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a function that returns a list"""
+
+
+def lookup(obj):
+    """This function returns a list"""
+    my_list = perform_lookup(obj)
+    return my_list
