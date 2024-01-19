09c38e2bed94827a4c571248ec61011e4244120c
chdrchz <saavannaa.rose@gmail.com>
2024-01-19 03:03:08 +0000

    testing documentation?

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a function that returns a list"""
+"""
+This module defines a function that returns a list
+"""
 
 
 def lookup(obj):
-    """This function returns a list"""
     methods_attributes = dir(obj)
     return methods_attributes
