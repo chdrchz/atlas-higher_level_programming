cede6b5e934ed03871f56015f4151736278f11bb
chdrchz <saavannaa.rose@gmail.com>
2024-01-22 11:16:58 -0800

    test

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """public instance method that validates value"""
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
