88e8f530787e4080f9d59bac5d707408072a9800
chdrchz <saavannaa.rose@gmail.com>
2024-01-22 10:26:59 -0800

    fixing tabs in method

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,5 +13,5 @@ class Rectangle(BaseGeometry):
 
     def integer_validator(value, name):
         """This defines a method that validates value that is inherited"""
-    if not isinstance(value, int) or value <= 0:
-        raise ValueError("{} must be a positive integer".format(name))
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("{} must be a positive integer".format(name))
