36a1ba0293651ee7857bfb8f0ad3d03ade3c1166
chdrchz <saavannaa.rose@gmail.com>
2024-01-22 08:31:01 -0800

    adding class and public method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """This defines a class named BaseGeometry"""
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
