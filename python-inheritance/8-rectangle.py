e8a229b3fab7574e2ffd1edbaa1a36ba15437af4
chdrchz <saavannaa.rose@gmail.com>
2024-01-22 10:23:11 -0800

    removing commas in documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a class named Rectangle, inherited"""
+"""This module defines a class named Rectangle that is inherited"""
 
 
 class Rectangle(BaseGeometry):
-    """This defines a class Rectangle, inherited"""
+    """This defines a class Rectangle that is inherited"""
     def __init__(self, width, height):
         """Instantiation method"""
         integer_validator(width, 'width')
@@ -12,6 +12,6 @@ class Rectangle(BaseGeometry):
         self._height = height
 
     def integer_validator(value, name):
-        """This defines a method that validates value, inherited"""
+        """This defines a method that validates value that is inherited"""
     if not isinstance(value, int) or value <= 0:
         raise ValueError("{} must be a positive integer".format(name))
