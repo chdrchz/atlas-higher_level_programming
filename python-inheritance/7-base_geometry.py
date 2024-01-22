47f7f24d4ea247fdb92648fbf6c2fd1d31a86c4d
chdrchz <saavannaa.rose@gmail.com>
2024-01-22 10:15:36 -0800

    adding f formatted strings

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """public instance method that validates value"""
-        if isinstance(value, int):
-            raise TypeError("value must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("value must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
