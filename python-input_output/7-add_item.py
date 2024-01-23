0e8844db9142bcce581f093d701e9dbf83ed02e3
chdrchz <saavannaa.rose@gmail.com>
2024-01-23 13:58:46 -0800

    adding load function

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-This module defines a function that adds all arguments to a python list
+This module defines a function that adds all arguments to a Python list
 then saves them to a file
 """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 
-
 file_name = "add_item.json"
-my_list = []
 my_list = load_from_json_file(file_name)
 
 for f in range(1, len(sys.argv)):
     my_list.append(sys.argv[f])
-    save_to_json_file(my_list, file_name)
+
+save_to_json_file(my_list, file_name)
