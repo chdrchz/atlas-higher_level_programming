571de20f19359c643917f0cb6512a85d595dbb64
chdrchz <saavannaa.rose@gmail.com>
2024-01-23 14:02:11 -0800

    initializing my_list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 
 file_name = "add_item.json"
+my_list = []
 
 try:
     my_list = load_from_json_file(fname)
