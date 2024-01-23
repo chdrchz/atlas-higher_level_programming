c0909d129d8a967d07ffcbc8a636a77263a132b1
chdrchz <saavannaa.rose@gmail.com>
2024-01-23 14:12:33 -0800

    removing whitespace

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ with simple data structure for JSON serialization of an object
 
 def class_to_json(obj):
     """
-    This function returns the dictionary description 
+    This function returns the dictionary description
     with simple data structure
     """
     return vars(obj)
