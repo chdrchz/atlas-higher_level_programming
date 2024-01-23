19d880bc8f3e027182d739368a948fefc764be81
chdrchz <saavannaa.rose@gmail.com>
2024-01-23 13:10:57 -0800

    redoing logic. write already returns the chars written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,8 +6,5 @@ def write_file(filename="", text=""):
     """
     This function writes to a file and returns the number of new chars
     """
-    new_chars = 0
-    with open(filename, encoding="utf-8") as file:
-        file.write(text)
-        new_chars = len(text)
-        return(new_chars)
+    with open(filename, mode='w', encoding="utf-8") as f:
+        return f.write(text)
