f2b8e7e53a1f33e75fd88eea699e2d7f61279b90
chdrchz <saavannaa.rose@gmail.com>
2024-01-23 16:16:29 -0800

    changing brackets to parenthesis

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -14,7 +14,7 @@ def pascal_triangle(n):
     for i in range(n):
         row = [1] * (i + 1)
         if i >= 2:
-            for j in range[1, i]:
+            for j in range(1, i):
                 row[j] = new_list[i - 1][j - 1] + new_list[i - 1][j]
         new_list.append(row)
     return new_list
