a1af8a01cbbc8b4fc0adb8a9badc556190cca35d
chdrchz <saavannaa.rose@gmail.com>
2024-01-25 12:31:32 -0600

    hehe changed x to y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,7 +74,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("y must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
