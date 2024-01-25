b4c669fa8c15ed6132863b8764cb92d068d5b7c0
chdrchz <saavannaa.rose@gmail.com>
2024-01-25 10:36:15 -0600

    adding else

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,10 +5,11 @@
 class Base:
     """This is a class named Base"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """This method is the class constructor"""
         if id is not None:
             self.id = id
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
