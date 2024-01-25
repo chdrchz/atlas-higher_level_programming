35887947cd86050f13292c3cded5fe75b7a29baa
chdrchz <saavannaa.rose@gmail.com>
2024-01-25 14:55:02 -0600

    adding new line at end of file

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """This method creates new instances of Square"""
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
