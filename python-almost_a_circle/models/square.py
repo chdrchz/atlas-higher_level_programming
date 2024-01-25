cc642990091a83fd62464d7246b8aef9f73525bf
chdrchz <saavannaa.rose@gmail.com>
2024-01-25 14:52:45 -0600

    changing backslash in formatted string in __str_ method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,5 +10,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
     
     def __str__(self):
-        return "[Square] ({}) {}\{} - {}".format(
+        return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
