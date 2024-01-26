a1e11fbb7639fc437bcadcc2e5e89e5e3021bfba
chdrchz <saavannaa.rose@gmail.com>
2024-01-25 18:06:15 -0600

    removing quotes around empty list return in from_json_string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,6 +38,6 @@ class Base:
     def from_json_string(json_string):
         """This method returns the list of the JSON string representation"""
         if json_string is None or not json_string:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
