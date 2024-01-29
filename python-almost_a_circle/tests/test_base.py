ae2ed6a6c4889e6124579459f5c6013a75f44a83
chdrchz <saavannaa.rose@gmail.com>
2024-01-29 14:46:35 -0600

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,69 +1,50 @@
 #!/usr/bin/python3
+"""
+Unittest module for Base class.
+"""
 import unittest
-import os
 from models.base import Base
 from unittest.mock import patch
 from io import StringIO
 
 
-class TestBaseMethods(unittest.TestCase):
+class TestBase(unittest.TestCase):
+    """
+    class for testing Base class
+    """
+    def test_auto_id_assign(self):
+        """Auto assign ID exists"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(99)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 99)
 
-    def test_init(self):
-        obj1 = Base()
-        self.assertEqual(obj1.id, 1)
+    def test_id_type(self):
+        """test the type of id to ensure an integer"""
+        base = Base(99)
+        self.assertIsInstance(base.id, int)
 
-        obj2 = Base()
-        self.assertEqual(obj2.id, 2)
+    def test_to_json_empty(self):
+        """test empty to json"""
+        with self.assertRaises(TypeError):
+            b1 = Base()
+            json_dictionary = Base.to_json_string()
 
-        obj3 = Base(100)
-        self.assertEqual(obj3.id, 100)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_to_json_string_with_none(self, mock_stdout):
+        expected_output = "[]"
+        result = Base.to_json_string(None)
+        self.assertEqual(result, expected_output)
+        self.assertEqual(mock_stdout.getvalue(), "")
 
-    def test_to_json_string(self):
-        obj = Base(1)
-        obj_dict = obj.to_dictionary()
-        json_string = Base.to_json_string([obj_dict])
-        self.assertEqual(json_string, '[{"id": 1}]')
-
-    def test_save_to_file(self):
-        obj1 = Base(1)
-        obj2 = Base(2)
-        Base.save_to_file([obj1, obj2])
-
-        with open("Base.json", "r") as file:
-            content = file.read()
-            self.assertEqual(content, '[{"id": 1}, {"id": 2}]')
-
-        # Clean up
-        os.remove("Base.json")
-
-    def test_from_json_string(self):
-        json_string = '[{"id": 1}, {"id": 2}]'
-        obj_list = Base.from_json_string(json_string)
-        self.assertEqual(len(obj_list), 2)
-        self.assertEqual(obj_list[0].id, 1)
-        self.assertEqual(obj_list[1].id, 2)
-
-    def test_create(self):
-        obj_dict = {"id": 1}
-        obj = Base.create(**obj_dict)
-        self.assertEqual(obj.id, 1)
-
-    def test_load_from_file(self):
-        obj1 = Base(1)
-        obj2 = Base(2)
-        Base.save_to_file([obj1, obj2])
-
-        loaded_objs = Base.load_from_file()
-        self.assertEqual(len(loaded_objs), 2)
-        self.assertEqual(loaded_objs[0].id, 1)
-        self.assertEqual(loaded_objs[1].id, 2)
-
-        # Clean up
-        os.remove("Base.json")
-
-    def test_load_from_file_missing_file(self):
-        loaded_objs = Base.load_from_file()
-        self.assertEqual(len(loaded_objs), 0)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_from_json_string_with_none(self, mock_stdout):
+        expected_output = []
+        result = Base.from_json_string(None)
+        self.assertEqual(result, expected_output)
+        self.assertEqual(mock_stdout.getvalue(), "")
 
 if __name__ == '__main__':
     unittest.main()
