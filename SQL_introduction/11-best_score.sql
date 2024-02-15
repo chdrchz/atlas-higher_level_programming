123a033156c3f523d16c4a6784db276675a25860
chdrchz <saavannaa.rose@gmail.com>
2024-02-15 15:53:07 -0600

    didnt add score before DESC

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- This script lists all records in a specific order by desc sort
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY DESC; 
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC; 
