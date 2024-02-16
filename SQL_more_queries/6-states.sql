a5bb6c95764c6e6b33bd635d703a744e08edc33d
chdrchz <saavannaa.rose@gmail.com>
2024-02-16 11:44:31 -0600

    I created the table, but didn't specify what database it belongs to

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- This script creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     ID INT DEFAULT 1 AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
