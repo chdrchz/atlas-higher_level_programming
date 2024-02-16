ee349fbd94499f51060d417f50efc59a3ee4e2bb
chdrchz <saavannaa.rose@gmail.com>
2024-02-16 12:05:50 -0600

    changing default 1 to not null

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- This script creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT DEFAULT 1 AUTO_INCREMENT UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 )
