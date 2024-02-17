f4571fa5a9b7a8ce50e85b0e5bd92c6340e944de
chdrchz <saavannaa.rose@gmail.com>
2024-02-17 11:50:36 -0600

    changing state.name to states.name

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Script that lists all cities in a database
-SELECT cities.id, cities.name, state.name FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 LEFT JOIN states ON cities.state_id = states.id ORDER BY id ASC;
