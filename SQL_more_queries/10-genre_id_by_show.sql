ad99ccaa15f4aac4196c1f386eb57a1fbebcd485
chdrchz <saavannaa.rose@gmail.com>
2024-02-17 11:57:59 -0600

    adding script that lists linked data

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- This script finds a specific type of data that is linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows_id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
