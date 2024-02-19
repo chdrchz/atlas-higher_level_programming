ac93c2d3a1ff98c70e9d997b4722dc548f534c50
chdrchz <saavannaa.rose@gmail.com>
2024-02-19 11:36:30 -0600

    testing changes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,22 +4,28 @@
 import MySQLdb
 import sys
 
-"""Create an engine"""
-engine= MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], engine=sys.argv[3])
+def list_states():
+    """This method creates an engine, and queries data"""
+    
+    """Create an engine"""
+    engine= MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                            passwd=sys.argv[2], engine=sys.argv[3])
 
-"""Create a cursor object"""
-cursor = engine.cursor()
+    """Create a cursor object"""
+    cursor = engine.cursor()
 
-"""Query the data to find all states"""
-cursor.execute("SELECT * FROM states ORDER BY id")
+    """Query the data to find all states"""
+    cursor.execute("SELECT * FROM states ORDER BY id")
 
-"""Fetch all data from result set"""
-rows = cursor.fetchall()
+    """Fetch all data from result set"""
+    rows = cursor.fetchall()
 
-"""Print the rows"""
-for row in rows:
-    print(row)
+    """Print the rows"""
+    for row in rows:
+        print(row)
 
-cursor.close()
-engine.close()
+    cursor.close()
+    engine.close()
+
+if __name__ == "__main__":
+    list_states()
