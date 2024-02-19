8324f4c1a09205ae8e4324bf36c2bda14b07ffc2
chdrchz <saavannaa.rose@gmail.com>
2024-02-19 13:16:44 -0600

    making line smaller to pass style check

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a function that establishes a connection to a SQL server"""
+"""This module defines a function that creates a connection to a SQL server"""
 
 import MySQLdb
 import sys
