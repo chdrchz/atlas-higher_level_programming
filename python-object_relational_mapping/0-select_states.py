d7553baaaf0af5e2d8104eaeaafd72da9b262dc0
chdrchz <saavannaa.rose@gmail.com>
2024-02-19 11:38:14 -0600

    adding a space before equals sign

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ def list_states():
     """This method creates an engine, and queries data"""
     
     """Create an engine"""
-    engine= MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                            passwd=sys.argv[2], engine=sys.argv[3])
+    engine = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                            password=sys.argv[2], engine=sys.argv[3])
 
     """Create a cursor object"""
     cursor = engine.cursor()
