0b861870705195ad618056c10578ac4054cbcc1f
chdrchz <saavannaa.rose@gmail.com>
2024-02-20 12:47:46 -0600

    okay now is it shorter?

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -28,7 +28,8 @@ if __name__ == "__main__":
     session = Session()
 
     """Query the data for "a" and list the result, then print"""
-    states_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_a = session.query(State)\
+                  .filter(State.name.like('%a%')).order_by(State.id).all()
     for state in states_a:
         print("{}: {}".format(state.id, state.name))
 
