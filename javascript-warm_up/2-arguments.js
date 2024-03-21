6b00c6e471a39ca54aaae20ee0b22e953fc42bb0
chdrchz <saavannaa.rose@gmail.com>
2024-03-21 16:38:00 -0500

    removing .length from ifs

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args_count = process.argv.length - 2 
 
-if (args_count.length === 0) {
+if (args_count === 0) {
     console.log('No argument');
-} else if (args_count.length === 1) {
+} else if (args_count === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
