4754aa77a5010d69c3ad6efbdc7f4e0b0cc6f7cf
chdrchz <saavannaa.rose@gmail.com>
2024-03-21 16:37:08 -0500

    changing arguments to args_count

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args_count = process.argv.length - 2 
 
-if (arguments.length === 0) {
+if (args_count.length === 0) {
     console.log('No argument');
-} else if (arguments.length === 1) {
+} else if (args_count.length === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
