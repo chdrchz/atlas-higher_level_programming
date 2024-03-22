fc69c7aad1427bad46120b1f8720eee0274d14ee
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 11:02:43 -0500

    style guide fixes

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const array = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
-  }
+}
