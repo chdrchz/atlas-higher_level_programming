5975cdddd2cac09d40a19ab4fd91ee7c9a3182d4
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 13:34:52 -0500

    fixing style guide, also I did not solve this recursively. oops

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const a = parseInt(process.argv[2]);
 
-function factorial(a) {
+function factorial (a) {
   let product = 1;
-  for (i = 1; i <= a; i++) {
+  for (let i = 1; i <= a; i++) {
     product *= i;
   }
   console.log(product);
