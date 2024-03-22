bffda53600737649add15637970be9102f5c2469
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 13:07:22 -0500

    setting a and b to be arguments

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add(a, b) {
   const sum = a + b;
-  console.log(sum);
+  return(sum);
 }
+
+console.log(add(a, b));
