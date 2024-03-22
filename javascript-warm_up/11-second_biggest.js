4db68f2c6b7055f8e357b5fe99417802701df8bd
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 13:51:37 -0500

    adding second biggest function

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+function second_biggest(args) {
+  if (args.length === 0) {
+    console.log(0);
+    return;
+  }
+  if (args.length === 1) {
+    console.log(0);
+    return;
+  }
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
+}
