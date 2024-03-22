5d05ab184081a72e3dddbdafb0cb5213f54d0153
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 11:22:01 -0500

    tetsing removing = to size

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(size)) {
   console.log('Missing size')
 }
 else { 
-  for (let i = 0; i <= size; i++) {
+  for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size) + '\n');
   }
 }
