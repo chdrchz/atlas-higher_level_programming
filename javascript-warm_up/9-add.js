2759090e806b99baab61870fc42fce4a5e42fb13
chdrchz <saavannaa.rose@gmail.com>
2024-03-22 13:10:57 -0500

    semistandard fix

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   const sum = a + b;
-  return(sum);
+  return (sum);
 }
 
 console.log(add(a, b));
