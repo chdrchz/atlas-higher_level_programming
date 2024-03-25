586a4f018a0271cfbb7cd9c32b8b73c06cffc4a7
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:16:12 -0500

    testing a to string return to get Retangle to pint on the output

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-        return {};
+        return { toString: () => 'Rectangle {}' };
     }
     this.width = w;
     this.height = h;
