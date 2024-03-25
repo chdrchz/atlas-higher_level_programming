d6182ada2f767637cc6cfbe4ec99a5b542c3b468
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:02:54 -0500

    testing change to logic in constructor method to print class name as well as return an empty object

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -2,11 +2,13 @@
 class Rectangle {
   // initialization method
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w > 0 || h > 0) {
+        this.width = w;
+        this.height = h;
+    }
+    else {
       return {};
     }
-    this.width = w;
-    this.height = h;
   }
 
   // print method to print a rectangle
