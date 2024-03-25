5e25a1b68fa42c76b5cc50ce5ccdc151eaa0629b
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:09:14 -0500

    moving error handling to the end to test

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
+    this.width = w;
+    this.height = h;
     if (width <= 0 || height <= 0) {
         return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
