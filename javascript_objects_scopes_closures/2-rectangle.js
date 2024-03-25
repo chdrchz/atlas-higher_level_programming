dfafd008d6a50c09fe7e7ca1fab3f1cdcff2a00b
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:08:10 -0500

    adding empty object initialization if width or height are <= 0

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (width <= 0 || height <= 0) {
+        return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
