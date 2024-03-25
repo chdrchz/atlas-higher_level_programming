e63f441495b87ae855a88c14dcd820a6133367e2
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:38:49 -0500

    reversing a list

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+    let left = 0;
+    let right = list.length - 1;
+    while (left < right) {
+        let temp = list[left];
+        list[left] = list[right];
+        list[right] = temp;
+        left++;
+        right--;
+    }
+    return list;
+}
