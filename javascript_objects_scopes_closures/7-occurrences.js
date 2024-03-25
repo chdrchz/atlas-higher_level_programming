d15e5cb53bbb21aa3a8154ce698abd6c0553aa8b
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:30:18 -0500

    adding script that searches a list and xounts how many times a specific data type is encountered

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+    for (let i = 0; i < list; i++) {
+      if (searchElement === list[i]) {
+        let occurences;
+        occurences += 1;
+      }
+      return occurences;
+    }
+}
