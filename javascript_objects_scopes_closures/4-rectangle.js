e4ceb6312a35e8a5a4d26fca77659adc67e0a0db
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:40:20 -0500

    adding rotate and double methods

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+class Rectangle {
+    // initialization method
+    constructor (w, h) {
+      if (w <= 0 || h <= 0) {
+        return {};
+      }
+      this.width = w;
+      this.height = h;
+    }
+  
+    // print method to print a rectangle
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+
+    // rotate method that switches width and height
+    rotate () {
+      this.width = h;
+      this.height = w;
+    }
+
+    // double method that multiples w + h by 2
+    double () {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+  module.exports = Rectangle;
