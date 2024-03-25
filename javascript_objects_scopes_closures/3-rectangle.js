f850b6e02935542d985d19c804ce27103a34f99c
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:36:48 -0500

    semistandard fix

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-    // initialization method
-    constructor (w, h) {
-      if (w <= 0 || h <= 0) {
-          return {};
-      }
-      this.width = w;
-      this.height = h;
+  // initialization method
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
     }
-    // print method to print a rectangle
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+    this.width = w;
+    this.height = h;
+  }
+
+  // print method to print a rectangle
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-  module.exports = Rectangle;
+}
+module.exports = Rectangle;
