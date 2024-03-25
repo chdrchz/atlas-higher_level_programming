0ad472cf8d0447b79b8327092bfa34f5b449e1e9
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:32:58 -0500

    defining occurences outside of for loop

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+    let occurences = 0;
     for (let i = 0; i < list.length; i++) {
       if (searchElement === list[i]) {
-        let occurences;
         occurences += 1;
       }
       return occurences;
