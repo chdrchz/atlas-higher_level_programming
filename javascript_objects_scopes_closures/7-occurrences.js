20444a78ba2cc274a9a160585c30417772dae65b
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:34:31 -0500

    moving return statement outside of for loop

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -5,6 +5,6 @@ exports.nbOccurences = function (list, searchElement) {
       if (searchElement === list[i]) {
         occurences += 1;
       }
-      return occurences;
     }
+    return occurences;
 }
