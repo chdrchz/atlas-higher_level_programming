fb077a6fb7f4db2ed312ffaabcf81d278497f1e6
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 12:21:18 -0500

    adding c print method that prints the square with a c. if c is undefined, prints an x instead

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  // initialization method
+  constructor (size) {
+    super(size, size);
+  }
+
+  // print method that changes char printed from x to c
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+module.exports = Square;
