c2edf938f265228ae51e8d947d9585adf904c9c2
chdrchz <saavannaa.rose@gmail.com>
2024-03-25 10:32:23 -0500

    testing printing rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+class Rectangle {
+    // initialization method
+    constructor (w, h) {
+      if (w <= 0 || h <= 0) {
+          return {};
+      }
+      this.width = w;
+      this.height = h;
+    }
+    // print method to print a rectangle
+    print (w, h) {
+      for (let i = 0; i < h; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+  module.exports = Rectangle;
