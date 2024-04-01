10e81e4c9d5aafac237780b488fc7218b8bb0366
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 10:36:21 -0500

    semistandard fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,8 +5,8 @@ const apiUrl = process.argv[2];
 const file = process.argv[3];
 
 request(apiUrl, function (err, response, body) {
+  if (err) throw err;
+  fs.writeFile(file, body, function (err) {
     if (err) throw err;
-    fs.writeFile(file, body, function (err) {
-      if (err) throw err;
-    });
   });
+});
