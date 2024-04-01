a7c04480df7ed5a652e607e7a156af6e166cb4b0
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 09:35:18 -0500

    semistandard fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const movieId = process.argv[2];
 const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
 request(apiUrl, function (err, response, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).title);
-  });
+  if (err) throw err;
+  console.log(JSON.parse(body).title);
+});
