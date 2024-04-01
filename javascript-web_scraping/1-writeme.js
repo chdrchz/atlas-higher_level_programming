565a831a8e61ef0d20af2cf2a4bab401839bf973
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 09:26:03 -0500

    changing variable names to be more descriptive

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const text = process.argv[3];
+const filePath = process.argv[2];
+const textWritten = process.argv[3];
 
-fs.writeFile(file, text, function (err) {
+fs.writeFile(filePath, textWritten, function (err) {
   if (err) throw err;
 });
