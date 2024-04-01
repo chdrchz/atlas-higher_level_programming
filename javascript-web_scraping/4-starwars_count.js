904ea787b2ad429ac514a55c925ca1a72ef84970
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 10:23:32 -0500

    semistandard fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,20 +5,20 @@ const filmApiUrl = process.argv[2];
 const characterApiUrl = 'https://swapi-api.hbtn.io/api/people/';
 
 request(filmApiUrl, function (err, response, body) {
-    if (!err && response.statusCode === 200) {
-        // Convert the data to JSON
-        const films = JSON.parse(body).results;
-        // Initialize movieCount
-        let movieCount = 0;
-        // Iterating over each film to find matching character id
-        films.forEach(function (film) {
-            // Find the characters for each movie
-            const characters = film.characters;
-            // Check if the characterId exists in the characters array
-            if (characters.includes(characterApiUrl + characterId)) {
-                movieCount++;
-            }
-        });
-        console.log(movieCount);
-    }
+  if (!err && response.statusCode === 200) {
+    // Convert the data to JSON
+    const films = JSON.parse(body).results;
+    // Initialize movieCount
+    let movieCount = 0;
+    // Iterating over each film to find matching character id
+    films.forEach(function (film) {
+      // Find the characters for each movie
+      const characters = film.characters;
+      // Check if the characterId exists in the characters array
+      if (characters.includes(characterApiUrl + characterId)) {
+        movieCount++;
+      }
+    });
+    console.log(movieCount);
+  }
 });
