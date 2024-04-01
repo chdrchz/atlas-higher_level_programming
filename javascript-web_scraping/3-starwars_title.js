bd4e48a3acdc54b816222defba2021e4ea984ed7
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 09:34:44 -0500

    finding a specific title based on an id

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(apiUrl, function (err, response, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).title);
+  });
