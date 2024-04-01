8421a12b06df1e8ffb7594a180954aeb67dc5866
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 18:35:58 -0500

    testing counting the number of tass associated with a user

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (err, response, body) {
+  if (err) throw err;
+  const users = {};
+  // Converts the response body to JSON and iterates over tasks
+  for (const task of JSON.parse(body)) {
+    // Checks if the task is completed and associated with a user
+    if (task.completed && task.userId) {
+      // If the user already exists in the users object, increment the count
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        // If the user doesn't exist, initialize the count to 1
+        users[task.userId] = 1;
+      }
+    }
+  }
+  console.log(users);
+});
