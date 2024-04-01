531ad75574ddb66117e6d3fbb2ccfa4e35ede7be
chdrchz <saavannaa.rose@gmail.com>
2024-04-01 09:31:30 -0500

    semistandard fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
-    if (err) throw err;
-    console.log('code:', response.statusCode);
-  });
+  if (err) throw err;
+  console.log('code:', response.statusCode);
+});
