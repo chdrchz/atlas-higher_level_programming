439ff854d328373c61d79e692c7cd7ffdcbbff24
chdrchz <saavannaa.rose@gmail.com>
2024-04-02 18:59:04 -0500

    adding script that changes from green to red on click

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+$(document).ready(function() {
+    $('DIV#toggle_header').click(function () {
+        if ($(this).hasClass('red')) {
+          $(this).removeClass('red');
+          $(this).addClass('green');
+        } else {
+          $(this).removeClass('green');
+          $(this).addClass('red');
+        }
+    });
+});
