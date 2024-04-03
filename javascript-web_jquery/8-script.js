afa3e713db0447b9632f8b016b2aa4d069a2d447
chdrchz <saavannaa.rose@gmail.com>
2024-04-02 19:16:11 -0500

    adding script that lists all movies

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+$(document).ready(function() {
+    $.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function(data) {
+        const movies = data.results;
+        const list = $("#list_movies");
+        $.each(movies, function(index, movie) {
+            list.append("<li>" + movie.title + "</li>");
+        });
+    });
+});
